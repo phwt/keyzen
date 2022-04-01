493ef3b5283e6dfa874fdd745b2dccdea10c94ff
phwt.smwt <phwt.smwt@gmail.com>
2022-04-01 15:39:58 +0700

    feat: word generation logic

--- src/contexts/PracticeContext.tsx
+++ src/contexts/PracticeContext.tsx
@@ -4,23 +4,62 @@ import {
     useCallback,
     useContext,
     useEffect,
+    useMemo,
     useState,
 } from "react";
 import { useConfig } from "./PracticeConfigContext";
 
 const wordLength = 9;
+const minConsecutive = 5;
+
+interface Character {
+    value: string;
+    consecutive: number;
+}
 
 interface IPracticeContext {
     word: string;
     input: string;
     appendInput: (character: string) => void;
+    practiceCharacters: Character[];
 }
+
+const convertToCharacterStats = (characters: string): Character[] =>
+    characters.split("").map((c) => ({
+        value: c,
+        consecutive: 0,
+    }));
 
 const PracticeContext = createContext<IPracticeContext>({} as IPracticeContext);
 
 export const PracticeProvider = ({ children }: { children: ReactNode }) => {
     const { characters } = useConfig();
     const [input, setInput] = useState("");
+    const [level, setLevel] = useState(1);
+    const [practiceCharacterStats, setPracticeCharacterStats] = useState<
+        Character[]
+    >(convertToCharacterStats(characters));
+
+    useEffect(() => {
+        setPracticeCharacterStats(convertToCharacterStats(characters));
+    }, [characters]);
+
+    const trainingCharacters = useMemo(() => {
+        return practiceCharacterStats
+            .slice(0, level + 1)
+            .filter((c) => c.consecutive < minConsecutive)
+            .map((c) => c.value);
+    }, [level]);
+
+    const trainingWord = useMemo(() => {
+        let word = "";
+        const trainingLength = trainingCharacters.length;
+        for (let i = 0; i < wordLength; i++) {
+            word +=
+                trainingCharacters[Math.floor(Math.random() * trainingLength)];
+        }
+        return word;
+    }, [trainingCharacters]);
 
     const appendInput = useCallback(
         (character: string) => {
@@ -36,9 +75,10 @@ export const PracticeProvider = ({ children }: { children: ReactNode }) => {
     return (
         <PracticeContext.Provider
             value={{
-                word: "arst neio",
+                word: trainingWord,
                 input,
                 appendInput,
+                practiceCharacters: practiceCharacterStats,
             }}
         >
             {children}
